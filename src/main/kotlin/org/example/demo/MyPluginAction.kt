270c42f3f2da979a564d59d4bd437dcfc720b8e0
surferaleksa <surferaleksa@gmail.com>
2026-05-20 02:13:52 +0200

    initial plugin skeleton

---
+++ src/main/kotlin/org/example/demo/MyPluginAction.kt
@@ -0,0 +1,10 @@
+package org.example.demo
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class MyPluginAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        // your logic here
+    }
+}
